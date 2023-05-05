c078e073ba29cc43d344fc5191b91fe93190ae5c
undertaker18 <jordanearlpascua28@gmail.com>
2023-05-06 01:44:17 +0800

    update

---
+++ database/seeders/XeroAccountsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class XeroAccountsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $xeroaccounts = [
+            [
+                'accounts_id' => '12345678',
+
+            ]
+        ];
+    }
+}
