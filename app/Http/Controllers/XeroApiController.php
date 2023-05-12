400356139ca22e74d467045c34df2bffe726361f
sarah <sarah.delos.santos@rarejob.ph>
2023-05-12 22:51:44 +0800

    migrate to client account

--- app/Http/Controllers/XeroApiController.php
+++ app/Http/Controllers/XeroApiController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\XeroAuth;
 use App\Models\XeroInvoice;
-use App\Models\XeroUsers;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
@@ -16,6 +15,7 @@ class XeroApiController extends Controller
 
     protected $clientId;
     protected $clientSecret;
+    protected $authotizarionBase64;
     protected $scope;
     protected $state;
     protected $authRedirectUri;
@@ -24,22 +24,15 @@ class XeroApiController extends Controller
     protected $grantType;
     protected $tenantId;
 
-    // const CLIENT_ID = '65E0B44A363548B5AEB2EC886C8E7CFB';
-    // const CLIENT_SECRET = 'mffcpfwfDsacHswX1hOilmpjPLQL0TLZ6usXJt7rRCF8PdcL';
-    // const REDIRECT_URL = 'http://localhost/index';
-    // const TENANT_ID = 'd510baa5-e646-44f8-acab-4d6fcac563d3';
-    // const STATE = 123;
-    // const SCOPE = 'offline_access accounting.transactions openid profile email accounting.contacts accounting.settings';
-
     public function __construct()
     {
-        $this->clientId = '0269B3AFB0BB40BB8D63883304C3F9D2';
-        $this->clientSecret = 'HzQisWkKOKuKkOCl_gJ2Ft4dcU7NisJ5s2gxxXp9oAjOeStP';
+
+        $this->clientId = '65E0B44A363548B5AEB2EC886C8E7CFB';
+        $this->clientSecret = '7b6hUvIwcB-Ox3oRv8qqpM6uVXE9xPT3jDWmEC88Pt9oSk25';
+        $this->authotizarionBase64 = 'NjVFMEI0NEEzNjM1NDhCNUFFQjJFQzg4NkM4RTdDRkI6N2I2aFV2SXdjQi1PeDNvUnY4cXFwTTZ1VlhFOXhQVDNqRFdtRUM4OFB0OW9TazI1';
         $this->scope = 'offline_access accounting.transactions openid profile email accounting.contacts accounting.settings';
         $this->state = '123';
-        $this->tenantId = '9b095c99-9f1d-4703-ab1e-27f968adebb5';
-        // $this->authRedirectUri = 'http://localhost:8000/api/v1/xero/token';
-        // $this->tokenRedirectUri = 'http://localhost:8000/api/v1/xero/token';
+        $this->tenantId = '56f717fe-5558-4932-9c1d-8be1da1264f2';
 
         $this->authRedirectUri = 'http://localhost:8000/v1/xero/token';
         $this->tokenRedirectUri = 'http://localhost:8000/v1/xero/token';
@@ -49,9 +42,6 @@ class XeroApiController extends Controller
 
     public function getAuth()
     {
-        // todo: use app
-        // $clientId = '65E0B44A363548B5AEB2EC886C8E7CFB';
-        // $clientSecret = 'mffcpfwfDsacHswX1hOilmpjPLQL0TLZ6usXJt7rRCF8PdcL';
         $authUrl = "https://login.xero.com/identity/connect/authorize?response_type=code&client_id=$this->clientId&scope=$this->scope&redirect_uri=$this->authRedirectUri";
 
         return redirect($authUrl);
@@ -60,11 +50,6 @@ class XeroApiController extends Controller
     public function getToken(Request $request)
     {
         $code = $request->input('code');
-        // $clientId = '65E0B44A363548B5AEB2EC886C8E7CFB';
-        // $clientSecret = 'mffcpfwfDsacHswX1hOilmpjPLQL0TLZ6usXJt7rRCF8PdcL';
-
-        // $scope = 'offline_access accounting.transactions openid profile email accounting.contacts accounting.settings';
-        // $state = '123';
 
         $this->grantType = 'authorization_code';
 
@@ -82,7 +67,7 @@ class XeroApiController extends Controller
             CURLOPT_HTTPHEADER => array(
                 'cache-control: no-cache',
                 'Content-Type: application/x-www-form-urlencoded',
-                'Authorization: Basic MDI2OUIzQUZCMEJCNDBCQjhENjM4ODMzMDRDM0Y5RDI6SHpRaXNXa0tPS3VLa09DbF9nSjJGdDRkY1U3TmlzSjVzMmd4eFhwOW9Bak9lU3RQ',
+                "Authorization: Basic $this->authotizarionBase64",
             ),
         ));
 
@@ -104,8 +89,7 @@ class XeroApiController extends Controller
         Session::put('xero_code', $code);
         Session::put('xero_expires_datetime', strtotime('+1800 seconds'));
         Session::put('xero_refresh_token', $responseArray['refresh_token']);
-
-        return;
+        return $response;
     }
 
     public function tokenRefresh()
@@ -129,7 +113,7 @@ class XeroApiController extends Controller
             CURLOPT_HTTPHEADER => array(
                 'cache-control: no-cache',
                 'Content-Type: application/x-www-form-urlencoded',
-                'Authorization: Basic MDI2OUIzQUZCMEJCNDBCQjhENjM4ODMzMDRDM0Y5RDI6SHpRaXNXa0tPS3VLa09DbF9nSjJGdDRkY1U3TmlzSjVzMmd4eFhwOW9Bak9lU3RQ',
+                "Authorization: Basic $this->authotizarionBase64",
                 'Cookie: _abck=64DAFBA79734E77CF5D24180B735F8A2~-1~YAAQD0IkFzH/46iHAQAAGsgO5gmPVeDPUk/WPPFx2xY1KLIy4DvxzmetqLEW9Ubc7jTUOB/227TALACfb4QXfjB0k+CSaBjyFo8+IVzFocljYnzKIj4eKin/7nVgdds4Otn+wsk1siuRSW4XKNLaEZqV4F99vYBqgmOd9wjCHOz6jaSR405N617zhGG2mDvfsdXzta7i0vDlE6UAo2qYllljQxtd4Su6H6Fui3b/2Iv7l/ANq0Y4SJ6ioxe2Sa340Al3g8VYkncSPPP+F0pa3gQrQ9itu0eV8VDTJtzRtEx8YRwEWz8XMIVvHaZ1wRFin++BzpB6q/E7A9gnUgR6SA6RV0fkGDZ/Df2q6BFpTNlZpwLZNbOQoz37Zw+cAoArnZG4Ukk=~-1~-1~-1; ak_bmsc=D9FAEC952203A5451F504B7C49254898~000000000000000000000000000000~YAAQRGjcF0ZivOaHAQAAlyvF5hPXy4JnplcXuGnO8QxUiAbQ8EP9s8f3RJ3m1OPlG0SzmqfrvV1zRzy3uVas3PJKAQveDStc1CsEC8Gzeje/Lo0G6zaArxAgjLSWJcU6Qet97Dvk4zSf56WrGCqjSvnbu+YpcKuhh4tlFJgRb1hQUMVwlWfqeqCAHIRvwvWT8c9wnczMjioZcTmDHmusBz7a2xyZA8xTh/rj7j/6V7Ig+gzNbEb5i4SkBRcJYR6BxQWYFPt4eGBXnkI2daWEbpzDGzsxYzK+LVY2NU9XaWw2nhYzF8gFziAvkP3ElFC53VBcmQQCiFrJTsZ+PGcYqXiqK99b9tQet+Iv6Gk3rCKSzhy0rCavqC2xc+xI9eg=; bm_sv=5DEE05F775AC70C13ED06CF323223340~YAAQp3TZF0aRf+aHAQAAnjWR5hP667jVS+zE+bgeFKQ9V/NJ51tDfUd1aguAQEunnLcruSHGLVdEM7ZyxX7J6LYPC/9p8hsDWWXnD0pgxX5GqOnC+C9FARnGZUo1ovL2HprxyODnFfyJatNCA5xdRe6MReHUZfi05F+BCkH9jrp2AXK51PQm1lP+Ei0SBk8lE9jGLN2oNNYTd8jL+Djq8+Kiw7W4i7RkfwLmmDG24jH9/xgkcSsflF3GRJEqPg==~1; bm_sz=116034C10A0844D12AC09CDCAA8255A2~YAAQTGjcF4D5P+SHAQAA9J/U5hPN/ynGClsPhnoOJkxtjxJOK4cfEQ+h1X/xdToBR8/HVF+EuuUQH1SYg3Po0bFPEmMTaJ40ZlfVpUwnKMJxb93DJczcxx4/MXIo3YagNQ5P4eiC3jc4hhTQYKhJtLcIx9nwnqWUxHs+bDSI4xO59FlorfFdNQFzs/qyQLCx+94OX/E0SnrucRsPXVnODgQL42xKpm9TZUcGrpAskBF2kYOLT3NgeI05I4XwCeUhEU92ZV7Jca3Wc/ZOWWTRAOUgySnqHB3FNym7L2GSsZbLia9SvUU7z64abb0u2v2xQHOPYySH0PX3~3752503~3420995; Device=9795df7db0b648b0a5b1ca5b22c78526'
             ),
         ));
@@ -182,14 +166,7 @@ class XeroApiController extends Controller
         $this->getRefreshToken();
         $curl = curl_init();
         $authorization = Session::get('xero_access_token_final');
-        $tenantId = '9b095c99-9f1d-4703-ab1e-27f968adebb5';
-        // $contactId = '85d15bf3-207f-4278-8449-e12dade98c66';
-        // $description = 'THIS IS A TEST';
-        // $amount = 20;
-        // $lineAmount = 20;
-        // $date = Carbon::now()->format('Y-m-d');
-        // $dueDate = Carbon::now()->addDay('1')->format('Y-m-d');;
-        // $reference = 'Xero - University Payment System';
+        $tenantId = $this->tenantId;
 
         $contactId = $params['contactId'];
         $description = $params['description'];
@@ -253,47 +230,42 @@ class XeroApiController extends Controller
     {
         $this->getRefreshToken();
 
-    //     $curl = curl_init();
-    //     $authorization = Session::get('xero_access_token_final');
-    //     curl_setopt_array($curl, array(
-    //         CURLOPT_URL => 'https://api.xero.com/api.xro/2.0/Invoices',
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => '',
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 0,
-    //         CURLOPT_FOLLOWLOCATION => true,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => 'PUT',
-    //         CURLOPT_POSTFIELDS =>'{ "Invoices": [ { "Type": "ACCREC", "Contact": { "ContactID": "85d15bf3-207f-4278-8449-e12dade98c66" }, "LineItems": [ { "Description": "THIS IS A TEST", "Quantity": 2, "UnitAmount": 20, "AccountCode": "200", "TaxType": "NONE", "LineAmount": 40 } ], "Date": "2023-05-04", "DueDate": "2023-12-10", "Reference": "Xero - University Payment System", "Status": "AUTHORISED" } ] }',
-    //         CURLOPT_HTTPHEADER => array(
-    //           'xero-tenant-id: 9b095c99-9f1d-4703-ab1e-27f968adebb5',
-    //           "Authorization: $authorization",
-    //           'Accept: application/json',
-    //           'Content-Type: application/json',
-    //           'Cookie: _abck=64DAFBA79734E77CF5D24180B735F8A2~-1~YAAQD0IkFzH/46iHAQAAGsgO5gmPVeDPUk/WPPFx2xY1KLIy4DvxzmetqLEW9Ubc7jTUOB/227TALACfb4QXfjB0k+CSaBjyFo8+IVzFocljYnzKIj4eKin/7nVgdds4Otn+wsk1siuRSW4XKNLaEZqV4F99vYBqgmOd9wjCHOz6jaSR405N617zhGG2mDvfsdXzta7i0vDlE6UAo2qYllljQxtd4Su6H6Fui3b/2Iv7l/ANq0Y4SJ6ioxe2Sa340Al3g8VYkncSPPP+F0pa3gQrQ9itu0eV8VDTJtzRtEx8YRwEWz8XMIVvHaZ1wRFin++BzpB6q/E7A9gnUgR6SA6RV0fkGDZ/Df2q6BFpTNlZpwLZNbOQoz37Zw+cAoArnZG4Ukk=~-1~-1~-1; ak_bmsc=32E7BDF6ED6F5FE5FB3C2907536A21A1~000000000000000000000000000000~YAAQS2jcFw0ONayHAQAAeMoO5hPKPg70B1CNZO/q3neu+CC8x7S2GwDSPKDpQ0SADARMPPT43/QPIXNOrsW5Fcr5xaj14nktkVkiYpCTdzmN4b8OzqdesZw5zYsHQBZKqNq5aO+cmgRf3iV+0YL3Ov4Tl7zV7hO3k7nYY7C7XS/3EAs9Fi/G3CSuTC1oG8HDk/dWNdqcqBenz20/dGnNnn+vY7wEe2XybQyXANJlw7qqEiIv9QZNVoZuskzPRBCcEtJXwl5FFscfFAF944eb+zXEwZgnZlDU5sBwdiU/NXi281C/zB10rni64M/KfGICixG0czTuh6AZERvkGF4GUiEqGw3Udo22DcdZRPwrxZeGyHMESYnWwvo=; bm_sv=240373A82D3A6511CD237D28E05C0B59~YAAQp3TZF++A/+WHAQAAF5Yx5hPEk7asjtsShHcArtxKyQMEgFzu1H8VOtMCNTxd6PXzxZ2ezGvnTRF73GCrGwFPm+3TR2PT8UzWQ72mJY4Bbe3ZHsRzKIlIfPqYTxWTLuraqUCULuJtup9HmfRyPLFnt7g/3rmKDCQHFscCEL4Xhh/QDtwJ6HqaYDXL6oUt48DCaGMqWh6vL4wEYjNHCbjW4ioQkIabcGcVAG91/U75Q83IBX7FC6Cf4fjRwO0=~1; bm_sz=517EC7602D2DC87B370B646AE3AC8A88~YAAQD0IkFzL/46iHAQAAGsgO5hPkvs/PGRUJ2attVsN7vS2RWYKOkfYzLGgjhRMvh7AQkXM0xgyn0Yp6tvOZANl536IigaIYE2lBUfTQWj6CL6Hk3PnhnbxrVWJomtxxq6vdtvybel8mWWUDSyRivyAXL3xijOZ02iR/wz7F5OnK3xjeTNOo6BjDjWgKjj73Qa9p3qpJK+KAWgG1QP5e4j5gkt7f5+MlvdKhWDkMNYwlgmIa3mJif5EszA68XUu/8HoyWsfKYbJnPX8tLXOgsi4GRRdl21CsZgU5ddEEah/y~4343353~3621955'
-    //         ),
-    //       ));
+        $curl = curl_init();
+        $authorization = Session::get('xero_access_token_final');
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.xero.com/api.xro/2.0/Invoices',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS =>'{ "Invoices": [ { "Type": "ACCREC", "Contact": { "ContactID": "85d15bf3-207f-4278-8449-e12dade98c66" }, "LineItems": [ { "Description": "THIS IS A TEST", "Quantity": 2, "UnitAmount": 20, "AccountCode": "200", "TaxType": "NONE", "LineAmount": 40 } ], "Date": "2023-05-04", "DueDate": "2023-12-10", "Reference": "Xero - University Payment System", "Status": "AUTHORISED" } ] }',
+            CURLOPT_HTTPHEADER => array(
+              'xero-tenant-id: 9b095c99-9f1d-4703-ab1e-27f968adebb5',
+              "Authorization: $authorization",
+              'Accept: application/json',
+              'Content-Type: application/json',
+              'Cookie: _abck=64DAFBA79734E77CF5D24180B735F8A2~-1~YAAQD0IkFzH/46iHAQAAGsgO5gmPVeDPUk/WPPFx2xY1KLIy4DvxzmetqLEW9Ubc7jTUOB/227TALACfb4QXfjB0k+CSaBjyFo8+IVzFocljYnzKIj4eKin/7nVgdds4Otn+wsk1siuRSW4XKNLaEZqV4F99vYBqgmOd9wjCHOz6jaSR405N617zhGG2mDvfsdXzta7i0vDlE6UAo2qYllljQxtd4Su6H6Fui3b/2Iv7l/ANq0Y4SJ6ioxe2Sa340Al3g8VYkncSPPP+F0pa3gQrQ9itu0eV8VDTJtzRtEx8YRwEWz8XMIVvHaZ1wRFin++BzpB6q/E7A9gnUgR6SA6RV0fkGDZ/Df2q6BFpTNlZpwLZNbOQoz37Zw+cAoArnZG4Ukk=~-1~-1~-1; ak_bmsc=32E7BDF6ED6F5FE5FB3C2907536A21A1~000000000000000000000000000000~YAAQS2jcFw0ONayHAQAAeMoO5hPKPg70B1CNZO/q3neu+CC8x7S2GwDSPKDpQ0SADARMPPT43/QPIXNOrsW5Fcr5xaj14nktkVkiYpCTdzmN4b8OzqdesZw5zYsHQBZKqNq5aO+cmgRf3iV+0YL3Ov4Tl7zV7hO3k7nYY7C7XS/3EAs9Fi/G3CSuTC1oG8HDk/dWNdqcqBenz20/dGnNnn+vY7wEe2XybQyXANJlw7qqEiIv9QZNVoZuskzPRBCcEtJXwl5FFscfFAF944eb+zXEwZgnZlDU5sBwdiU/NXi281C/zB10rni64M/KfGICixG0czTuh6AZERvkGF4GUiEqGw3Udo22DcdZRPwrxZeGyHMESYnWwvo=; bm_sv=240373A82D3A6511CD237D28E05C0B59~YAAQp3TZF++A/+WHAQAAF5Yx5hPEk7asjtsShHcArtxKyQMEgFzu1H8VOtMCNTxd6PXzxZ2ezGvnTRF73GCrGwFPm+3TR2PT8UzWQ72mJY4Bbe3ZHsRzKIlIfPqYTxWTLuraqUCULuJtup9HmfRyPLFnt7g/3rmKDCQHFscCEL4Xhh/QDtwJ6HqaYDXL6oUt48DCaGMqWh6vL4wEYjNHCbjW4ioQkIabcGcVAG91/U75Q83IBX7FC6Cf4fjRwO0=~1; bm_sz=517EC7602D2DC87B370B646AE3AC8A88~YAAQD0IkFzL/46iHAQAAGsgO5hPkvs/PGRUJ2attVsN7vS2RWYKOkfYzLGgjhRMvh7AQkXM0xgyn0Yp6tvOZANl536IigaIYE2lBUfTQWj6CL6Hk3PnhnbxrVWJomtxxq6vdtvybel8mWWUDSyRivyAXL3xijOZ02iR/wz7F5OnK3xjeTNOo6BjDjWgKjj73Qa9p3qpJK+KAWgG1QP5e4j5gkt7f5+MlvdKhWDkMNYwlgmIa3mJif5EszA68XUu/8HoyWsfKYbJnPX8tLXOgsi4GRRdl21CsZgU5ddEEah/y~4343353~3621955'
+            ),
+          ));
 
-    //     $response = curl_exec($curl);
+        $response = curl_exec($curl);
 
-    //     curl_close($curl);
+        curl_close($curl);
 
-    //     echo '<pre>';
-    //     echo $response;
-    //     $responseArray = json_decode($response, true);
-
+        echo '<pre>';
+        echo $response;
     }
 
     public function postPayment($params)
     {
         $this->getRefreshToken();
-
-        $curl = curl_init();
         $authorization = Session::get('xero_access_token_final');
-        $curl = curl_init();
-        $tenantId = '9b095c99-9f1d-4703-ab1e-27f968adebb5';
-        // params
+        $tenantId = $this->tenantId;
 
+        $curl = curl_init();
         $invoiceId = $params['invoiceId'];
         $amount = $params['amount'];
         $date = $params['date'];
@@ -338,12 +310,10 @@ class XeroApiController extends Controller
     public function postAccounts()
     {
         $this->getRefreshToken();
+        $authorization = Session::get('xero_access_token_final');
+        $tenantId = $this->tenantId;
 
         $curl = curl_init();
-        $authorization = Session::get('xero_access_token_final');
-        $curl = curl_init();
-        $invoiceId = 'afa5fdc0-a1b2-4e6a-85b1-a274466a4dca';
-        $tenantId = '9b095c99-9f1d-4703-ab1e-27f968adebb5';
         curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.xero.com/api.xro/2.0/Contacts',
         CURLOPT_RETURNTRANSFER => true,
@@ -407,12 +377,10 @@ class XeroApiController extends Controller
     {
         $this->getRefreshToken();
 
-        $email = 'pgw.2023.01@gmail.com';
-        $curl = curl_init();
         $authorization = Session::get('xero_access_token_final');
+        $tenantId = $this->tenantId;
+
         $curl = curl_init();
-        $invoiceId = 'afa5fdc0-a1b2-4e6a-85b1-a274466a4dca';
-        $tenantId = '9b095c99-9f1d-4703-ab1e-27f968adebb5';
         curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.xero.com/api.xro/2.0/Contacts',
         CURLOPT_RETURNTRANSFER => true,
