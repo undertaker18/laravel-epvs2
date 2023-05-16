94cf02218384904800abc19dc389569206c2224e
undertaker18 <jordanearlpascua28@gmail.com>
2023-05-16 17:13:58 +0800

    plugin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,27 +9,27 @@
         <!-- Google Font: Source Sans Pro -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/fontawesome-free/css/all.min.css') }}">
         <!-- Ionicons -->
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
         <!-- Tempusdominus Bootstrap 4 -->
-        <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
         <!-- iCheck -->
-        <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
         <!-- JQVMap -->
-        <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/jqvmap/jqvmap.min.css') }}">
         <!-- Theme style -->
-        <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/dist/css/adminlte.min.css') }}">
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
         <!-- Daterange picker -->
-        <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/daterangepicker/daterangepicker.css') }}">
         <!-- summernote -->
-        <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/summernote/summernote-bs4.min.css') }}">
        <!-- DataTables -->
-        <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/auth/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 
         <!-- Chart -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -55,50 +55,50 @@
 
 
                     <!-- jQuery -->
-            <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jquery/jquery.min.js') }}"></script>
 
             <!-- jQuery UI 1.11.4 -->
-            <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
             <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
             <script>
                 $.widget.bridge('uibutton', $.ui.button);
             </script>
             <!-- Bootstrap 4 -->
-            <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
             <!-- ChartJS -->
-            <script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/chart.js/Chart.min.js') }}"></script>
             <!-- Sparkline -->
-            <script src="{{ asset('plugins/sparklines/sparkline.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/sparklines/sparkline.js') }}"></script>
             <!-- JQVMap -->
-            <script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-            <script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
             <!-- jQuery Knob Chart -->
-            <script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
             <!-- daterangepicker -->
-            <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-            <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/moment/moment.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/daterangepicker/daterangepicker.js') }}"></script>
             <!-- Tempusdominus Bootstrap 4 -->
-            <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
             <!-- Summernote -->
-            <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/summernote/summernote-bs4.min.js') }}"></script>
             <!-- overlayScrollbars -->
-            <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
             <!-- AdminLTE App -->
-            <script src="{{ asset('dist/js/adminlte.js') }}"></script>
-            <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
+            <script src="{{ asset('assets/auth/dist/js/adminlte.js') }}"></script>
+            <script src="{{ asset('assets/auth/dist/js/pages/dashboard.js') }}"></script>
             <!-- DataTables & Plugins -->
-            <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-            <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
-            <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
-            <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-            <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/jszip/jszip.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/pdfmake/pdfmake.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/pdfmake/vfs_fonts.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+            <script src="{{ asset('assets/auth/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 
 
             <!-- ./wrapper -->
