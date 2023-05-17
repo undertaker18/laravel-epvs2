f603635f972898749a0a0abcea8332a3d2fe566b
undertaker18 <jordanearlpascua28@gmail.com>
2023-05-17 13:54:15 +0800

    Revert "commit"
    This reverts commit 4d3a7f4d924cd87d509c32df4074466bcff9ea22.

--- config/app.php
+++ config/app.php
@@ -159,8 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-        // ...
-        Yajra\DataTables\DataTablesServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
