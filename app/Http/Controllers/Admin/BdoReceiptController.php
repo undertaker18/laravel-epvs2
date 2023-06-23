4c9d86b89c55b5ea7d2beee9239310a75459ef35
undertaker18 <jordanearlpascua28@gmail.com>
2023-06-23 14:19:15 +0800

    commit

--- app/Http/Controllers/Admin/BdoReceiptController.php
+++ app/Http/Controllers/Admin/BdoReceiptController.php
@@ -23,51 +23,62 @@ class BdoReceiptController extends Controller
     }
     
     public function upload(Request $request)
-    {
-        $file = $request->file('csv_file');
-    
-        if (!$file) {
-            return redirect()->back()->withErrors(['Please upload a file.']);
-        }
-    
-        if (!in_array($file->getClientOriginalExtension(), ['csv', 'txt'])) {
-            return redirect()->back()->withErrors(['File must be a CSV or TXT.']);
-        }
-    
-        $handle = fopen($file->getPathname(), 'r');
-    
-        if (!$handle) {
-            return redirect()->back()->withErrors(['Unable to open file.']);
-        }
-    
-        $uploadedDescriptions = []; // Array to store uploaded descriptions
-    
-        while (($data = fgetcsv($handle)) !== false) {
-            $description = $data[2];
-    
-            // Check if the description already exists in the uploaded descriptions array
-            if (in_array($description, $uploadedDescriptions)) {
-                continue; // Skip uploading the record
-            }
-    
-            BdoReceipt::create([
-                'posting_datetime' => $data[0],
-                'branch' => $data[1],
-                'description' => $description,
-                'debit' => $data[3],
-                'credit' => $data[4],
-                'running_balance' => $data[5],
-                'check_number' => $data[6],
-            ]);
-    
-            $uploadedDescriptions[] = $description; // Add the description to the uploaded descriptions array
-        }
-    
-        fclose($handle);
-    
-        return redirect()->back()->with('success', 'File uploaded successfully!');
+{
+    $file = $request->file('csv_file');
+
+    if (!$file) {
+        return redirect()->back()->withErrors(['Please upload a file.']);
     }
+
+    if (!in_array($file->getClientOriginalExtension(), ['csv', 'txt'])) {
+        return redirect()->back()->withErrors(['File must be a CSV or TXT.']);
+    }
+
+    $handle = fopen($file->getPathname(), 'r');
+
+    if (!$handle) {
+        return redirect()->back()->withErrors(['Unable to open file.']);
+    }
+
+    while (($data = fgetcsv($handle)) !== false) {
+        if (count($data) < 7) {
+            // Handle invalid data format (less than 7 elements)
+            continue;
+        }
+
+        $description = isset($data[2]) ? $data[2] : null;
+
+        // Check if the description is set and is not empty
+        if (empty($description)) {
+            // Handle missing description
+            continue;
+        }
+
+        // Check if the record already exists in the database based on the description field
+        $existingRecord = BdoReceipt::where('description', $description)->first();
+        if ($existingRecord) {
+            continue; // Skip uploading the record
+        }
+
+        BdoReceipt::create([
+            'posting_datetime' => $data[0],
+            'branch' => $data[1],
+            'description' => $description,
+            'debit' => $data[3],
+            'credit' => $data[4],
+            'running_balance' => $data[5],
+            'check_number' => $data[6],
+        ]);
+    }
+
+    fclose($handle);
+
+    return redirect()->back()->with('success', 'File uploaded successfully!');
+}
+
     
+    
+        
 
     
 
